d7f0643f7e679863c6b5e7fc95be9681384dfb1b
李振邦 <lizhenbang56@163.com>
2021-01-13 15:38:30 +0800

    提交求职

---
+++ 博士/求职/刷题/iou.py
@@ -0,0 +1,20 @@
+def bb_intersection_over_union(boxA, boxB):  # xyxy
+    boxA = [int(x) for x in boxA]
+    boxB = [int(x) for x in boxB]
+
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    return iou
+
+res = bb_intersection_over_union([0, 0, 10, 10], [5, 5, 15, 15])
+print(res)
