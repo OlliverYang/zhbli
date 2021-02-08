001374c392d90cc39e3c88fd2d89d37ffe432151
李振邦 <lizhenbang56@163.com>
2021-02-08 10:42:39 +0800

    微调座右铭位置

--- 博士/programs/座右铭/note.py
+++ 博士/programs/座右铭/note.py
@@ -7,7 +7,7 @@ i=0
 top=Tk()
 lab=Label(top,text='欢迎使用！', font=('Times','15'), fg='white', bg='black')
 lab.master.overrideredirect(True)
-lab.master.geometry("+1000+1024")  # 左右、上下
+lab.master.geometry("+800+1040")  # 左右、上下
 lab.pack()
 words = ['不要猜测','自由','循序渐进', '对自己诚实', '不同的人有不同的感受', '睡觉时主动、自动想事情是生理疾病，但可以通过意识治标。']
 length = len(words)
