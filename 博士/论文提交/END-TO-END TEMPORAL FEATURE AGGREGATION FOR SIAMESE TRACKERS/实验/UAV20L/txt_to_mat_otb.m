16acd2e0b6fc24a89ccaddb046cea3ee4b52f9a6
李振邦 <lizhenbang56@163.com>
2021-02-24 15:52:01 +0800

    更新实验数据 PTAV.mat

--- 博士/论文提交/END-TO-END TEMPORAL FEATURE AGGREGATION FOR SIAMESE TRACKERS/实验/UAV20L/txt_to_mat_otb.m
+++ 博士/论文提交/END-TO-END TEMPORAL FEATURE AGGREGATION FOR SIAMESE TRACKERS/实验/UAV20L/txt_to_mat_otb.m
@@ -1,11 +1,11 @@
 clear;clc;
-path1 = '.\PTAV';
+path1 = '.\SiamRPN';
 filelist = dir(path1); %序列名
 [n,c] =size(filelist); %返回矩阵行,列，n为序列总数
 disp(n);disp(c);
  for i = 3:n;
      disp(filelist(i).name);
-     Newname=strcat(filelist(i).name(1:end-4),'_PTAV.mat'); %Basketball_ltdsst
+     Newname=strcat(filelist(i).name(1:end-4),'_SiamRPN.mat'); %Basketball_ltdsst
      matrix1 = importdata(fullfile(path1,filelist(i).name));
      shape = size(matrix1);
      len = shape(1);
