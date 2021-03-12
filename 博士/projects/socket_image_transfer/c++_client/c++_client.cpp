cb70d8fb16f0c12b114dd706bfe1fc3e0dd326b9
李振邦 <lizhenbang56@163.com>
2021-03-12 09:39:45 +0800

    跑通 windows c++ socket ipv6

--- 博士/projects/socket_image_transfer/c++_client/c++_client.cpp
+++ 博士/projects/socket_image_transfer/c++_client/c++_client.cpp
@@ -1,37 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <WinSock2.h>
-#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
 
+#include <winsock2.h>//winsock的头文件
+#include <ws2tcpip.h>//sockaddr_in6的头文件
+#include <iostream>
+using  namespace  std;
+
+//指定动态库的lib文件
+#pragma comment(lib,"ws2_32.lib")
+
+
+//TCP客户端
 int main() {
-	//初始化DLL
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	//创建套接字
-	SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//初始化winsock2.2相关的动态库
+	WSADATA  wd;//获取socket相关信息
+	if (WSAStartup(MAKEWORD(2, 2), &wd) != 0)//0表示成功
+	{
+		cout << "WSAStartup  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//向服务器发起请求
-	sockaddr_in sockAddr;
-	memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
-	sockAddr.sin_family = PF_INET;
-	sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	sockAddr.sin_port = htons(1234);
-	connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+	//1.创建TCP   socket , 流式套接字, AF_INET改为AF_INET6
+	SOCKET   s = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET) {
+		cout << "socket  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//接收服务器传回的数据
-	char szBuffer[MAXBYTE] = { 0 };
-	recv(sock, szBuffer, MAXBYTE, NULL);
+	//2.链接服务端
+	sockaddr_in6   addr;//不建议使用sockaddr，建议用sockaddr_in
+	memset(&addr, 0, sizeof(sockaddr_in6)); //重点，否则就10049错误
+	addr.sin6_port = htons(8890);//网络字节序 
 
-	//输出接收到的数据
-	printf("Message form server: %s\n", szBuffer);
+								 //addr.sin6_addr = inet_addr("127.0.0.1");//绑定指定地址， ipv4
+	inet_pton(AF_INET6, "0:0:0:0:0:0:0:1", &addr.sin6_addr);//绑定指定地址， ipv6  格式 /*"fe80::ce6:3cc:f93a:4203%5",*/
 
-	//关闭套接字
-	closesocket(sock);
+	addr.sin6_family = AF_INET6; //地址族
+	int len = sizeof(sockaddr_in6);//结构大小改变sizeof(sockaddr_in6)
 
-	//终止使用 DLL
+	if (connect(s, (sockaddr*)&addr, len) == SOCKET_ERROR) {
+		cout << "connect  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
+
+	//3.接受发送消息
+	int  ret = 0;
+	do {
+		//接受客户端的消息
+		char buf[64] = { '\0' };
+		ret = recv(s, buf, 64, 0);//把flag置0
+
+		char ipbuf[100] = { 0 };
+		inet_ntop(AF_INET6, (LPVOID)&addr.sin6_addr, ipbuf, 100);
+
+		cout << "recv	" << ipbuf << ":    " << buf << endl;// inet_ntoa转换为IP字符串
+		char *str = "I am Client!";
+		//发送
+		ret = send(s, str, strlen(str), 0);
+
+		Sleep(1000);
+	} while (ret != SOCKET_ERROR &&  ret != 0);
+
+
+	//4.关闭套接字
+	closesocket(s);
+
+	//清理winsock环境
 	WSACleanup();
 
-	system("pause");
-	return 0;
+
+	return   0;
 }
+
--- 博士/projects/socket_image_transfer/c++_server/c++_server.cpp
+++ 博士/projects/socket_image_transfer/c++_server/c++_server.cpp
@@ -1,41 +1,88 @@
-#include <stdio.h>
-#include <winsock2.h>
-#pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
+#include <winsock2.h>//winsock的头文件
+#include <ws2tcpip.h>//sockaddr_in6的头文件
+#include <iostream>
 
+using  namespace  std;
+
+//指定动态库的lib文件
+#pragma comment(lib,"ws2_32.lib")
+
+//TCP服务端IPv6版
 int main() {
-	//初始化 DLL
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	//创建套接字
-	SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//初始化winsock2.2相关的动态库
+	WSADATA  wd;//获取socket相关信息
+	if (WSAStartup(MAKEWORD(2, 2), &wd) != 0)//0表示成功
+	{
+		cout << "WSAStartup  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//绑定套接字
-	sockaddr_in sockAddr;
-	memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
-	sockAddr.sin_family = PF_INET;  //使用IPv4地址
-	sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-	sockAddr.sin_port = htons(1234);  //端口
-	bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+	//1.创建TCP   socket , 流式套接字   ， AF_INET改为AF_INET6
+	SOCKET   s = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET) {
+		cout << "socket  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//进入监听状态
-	listen(servSock, 20);
+	//2.绑定socket到一个IP地址和端口，  sockaddr_in改为sockaddr_in6
+	sockaddr_in6   addr;//不建议使用sockaddr，建议用sockaddr_in
+	memset(&addr, 0, sizeof(sockaddr_in6)); //重点，否则就10049错误
+	addr.sin6_port = htons(8890);//网络字节序
+	addr.sin6_family = AF_INET6; //地址族AF_INET改为AF_INET6
 
-	//接收客户端请求
-	SOCKADDR clntAddr;
-	int nSize = sizeof(SOCKADDR);
-	SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+								 //addr.sin6_addr = in6addr_any;//  把INADDR_ANY替换为in6addr_any，表示绑定任意ip 
+								 //addr.sin6_addr = inet_addr("127.0.0.1");//绑定指定地址， ipv4
+	inet_pton(AF_INET6, "0:0:0:0:0:0:0:1", &addr.sin6_addr);//绑定指定地址， ipv6 格式 /*"fe80::ce6:3cc:f93a:4203%5",*/
 
-	//向客户端发送数据
-	char *str = "Hello World!";
-	send(clntSock, str, strlen(str) + sizeof(char), NULL);
+	int len = sizeof(sockaddr_in6);//地址结构大小改变 sizeof(sockaddr_in6)
+	if (bind(s, (sockaddr *)&addr, len) == SOCKET_ERROR) {
+		cout << "bind  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//关闭套接字
-	closesocket(clntSock);
-	closesocket(servSock);
+	//3.监听, 5代表正在等待完成相应的TCP三路握手过程的队列长度
+	if (listen(s, 5) == SOCKET_ERROR) {
+		cout << "listen  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
 
-	//终止 DLL 的使用
+	//4.接受客户端请求，并且返回和客户端通讯的套接字，sockaddr_in改为sockaddr_in6
+	sockaddr_in6   addrClient;// 保存客户端IP地址端口 
+	memset(&addrClient, 0, sizeof(sockaddr_in6));
+	len = sizeof(sockaddr_in6);//地址结构大小改变 sizeof(sockaddr_in6)
+	SOCKET c = accept(s, (sockaddr*)&addrClient, &len);//成功返回套接字
+	if (c == INVALID_SOCKET) {
+		cout << "accept  error:" << WSAGetLastError() << endl;
+		return 0;
+	}
+
+	//5.发送，接受消息
+	int  ret = 0;
+	do {
+		char *str = "I am  Server!";
+		//向客户端发送数据,不能用监听套接字s，而应该用accept返回的套接字c
+		ret = send(c, str, strlen(str), 0);//把flag置0
+
+										   //接受客户端的消息
+		char buf[64] = { '\0' };
+		ret = recv(c, buf, 64, 0);//把flag置0
+
+		char ipbuf[100] = { 0 };
+		inet_ntop(AF_INET6, (LPVOID)&addrClient.sin6_addr, ipbuf, 100);
+
+		cout << "recv	" << ipbuf << ":    " << buf << endl;// inet_ntoa转换为IP字符串
+	} while (ret != SOCKET_ERROR &&  ret != 0);//对方关闭，返回0 ，错误返回SOCKET_ERROR
+
+
+											   //6.关闭套接字
+	closesocket(s);
+
+
+	//清理winsock环境
 	WSACleanup();
 
-	return 0;
+
+	return   0;
 }
+
